b59cfdc6e82aeb750a1e0c5db9cc2c9f26072a4c
Sumit Kumar <sumitkuo@in.ibm.com>
2025-12-18 20:28:22 +0530

    Bumping to 7.0.1.0 (#23)
    Signed-off-by: Sumit Kumar <sumitkuo@in.ibm.com>

--- classes/apimgr_yml.py
+++ classes/apimgr_yml.py
@@ -66,7 +66,7 @@ CONFIG_apimgr_YML = {
     'UTILITY_HOSTNAME': 'utilityBareMetal',
     'CAMPUS_INTERFACE': 'campus',
     'CAMPUS_INTERFACE_IP': '192.168.100.10',
-    'IMAGE_VERSION': '7.0.0.0',
+    'IMAGE_VERSION': '7.0.1.0',
 
     'API_PORT': '46443'
 }
@@ -513,10 +513,10 @@ class apimgr_yml(object):
                     "Going to ask the user for a Image Version"
                 )
                 IMAGE_VERSION_user = input(
-                    "Enter the image version (default: 7.0.0.0): "
+                    "Enter the image version (default: 7.0.1.0): "
                 )
                 if IMAGE_VERSION_user == "":
-                    IMAGE_VERSION_user = "7.0.0.0"
+                    IMAGE_VERSION_user = "7.0.1.0"
                     break
                 elif IMAGE_VERSION_user == "6.2.3.0" or \
                         IMAGE_VERSION_user == "6.2.3.1" or \
@@ -524,7 +524,7 @@ class apimgr_yml(object):
                         IMAGE_VERSION_user == "7.0.0.0":
                     break
                 else:
-                    print("Image name should be 6.2.3.0 or 6.2.3.1 or 6.2.3.2 or 7.0.0.0\n")
+                    print("Image name should be 6.2.3.0 or 6.2.3.1 or 6.2.3.2 or 7.0.0.0 or 7.0.1.0\n")
             return IMAGE_VERSION_user
         except KeyboardInterrupt:
             print("")
